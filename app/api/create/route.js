989c2792215345a752d4befd9ce16e1824cf38ac
Naman Srivastava <namansrivastava1608@gmail.com>
2025-06-10 21:45:44 +0530

    Webhooks set and middleware fixed

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,12 +1,13 @@
 import { StreamChat } from "stream-chat";
 const api_key = "n5kgyjq4t3at";
 const api_secret = "q4j6kxh4rzgtxqaaa4d3mqa2dyvx7w3q642sfk4xwkwsddq95fsgxpkpyhdrdfxd";
-export async function GET() {
+export async function POST(request) {
     const server_client = StreamChat.getInstance(
         api_key,
         api_secret
     )
-    const token = server_client.createToken("user_2yJJtj357KGbLdELpvPJjBiGGTb")
-    console.log(token)
+    const user = await request.json()
+    const token = server_client.createToken(user.data.id) //user id taken dyanmaicaly
+     console.log(token)
     return Response.json({ message: 'Hello World' })
 }
